beabf92ffc70db02e77a2f84476551d68fadf749
Md Ashikul Islam <akashashikul@gmail.com>
2026-08-29 18:37:26 +0600

    api update

--- routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyJWT } = require('../middleware/auth');
-const { editComment, deleteComment } = require('../controllers/commentController');
-
-router.patch('/:commentId', verifyJWT, editComment);
-router.delete('/:commentId', verifyJWT, deleteComment);
-
-module.exports = router;
